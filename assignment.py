4c85cdfeb60d7681793e682c10e976a82baf96c3
tishajanet <tishajanet@students.uonbi.ac.ke>
2025-09-20 21:00:59 +0300

    commit 1

---
+++ assignment.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Create two 2x2 arrays
+A = np.array([[1, 2],
+              [3, 4]])
+
+B = np.array([[5, 6],
+              [7, 8]])
+
+# Elementwise addition
+add_result = A + B
+
+# Elementwise multiplication
+mul_result = A * B
+
+# Matrix product
+matmul_result = A @ B   # or np.matmul(A, B)
+
+print("Array A:\n", A)
+print("Array B:\n", B)
+print("\nElementwise Addition:\n", add_result)
+print("\nElementwise Multiplication:\n", mul_result)
+print("\nMatrix Product (A @ B):\n", matmul_result)
